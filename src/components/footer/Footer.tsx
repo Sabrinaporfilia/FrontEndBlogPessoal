63ddf1ef9d74708d579cb53edf5409841f013174
Sabrina <sabrinaprogram99@gmail.com>
2023-07-28 14:44:17 -0300

    CRUD de Temas blog-pessoal

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
+
 function Footer() {
  
   
@@ -9,9 +10,11 @@ function Footer() {
     <>
         <div className="flex justify-center bg-indigo-900 text-white">
           <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: </p>
-            <p className='text-lg'>Acesse nossas redes sociais</p>
+            <p className='text-xl font-bold'>Blog pessoal Sabrina | Copyright: </p>
+            <p className='text-lg'>Acesse minhas redes sociais</p>
             <div className='flex gap-2'>
+
+              
               <LinkedinLogo size={48} weight='bold' />
               <InstagramLogo size={48} weight='bold' />
               <FacebookLogo size={48} weight='bold' />
