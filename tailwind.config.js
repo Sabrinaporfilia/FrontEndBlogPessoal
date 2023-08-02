11781835ea8050fd12bfdadeb8d8a098fa95499c
Sabrina <sabrinaprogram99@gmail.com>
2023-08-02 14:14:46 -0300

    CRUD de postagens

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      cursor: {
+        'fancy': 'url(.hand.cur), pointer',
+      }
+    },
   },
   plugins: [],
 }
